f03d43d19d5f71918014f7c789ca5ac4fa54c26d
Mark Lane Hamlin <mhamlin0621@proton.me>
2026-03-14 10:42:44 -0500

    3: Fixing the parenthesis in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
-print("Hello to my first solo project")
-print("I am making the game snake")
-print("The objective is to have square on a screen a grows each time the snake eats an apple")
+def main():
+    print("Hello to my first solo project")
+    print("I am making the game snake")
+    print("The objective is to have square on a screen a grows each time the snake eats an apple")
 
+if __name__ == "__main__":
+          main()
