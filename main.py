ecfb9bbae358dfac2dc657ad9621b690a91e0b26
Mark Lane Hamlin <mhamlin0621@proton.me>
2026-03-14 10:38:06 -0500

    Setting up my environment

---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame
+print("Hello to my first solo project")
+print("I am making the game snake")
+print("The objective is to have square on a screen a grows each time the snake eats an apple")
+
