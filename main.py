fb697aec330609e4c13e19455396479f988a71ba
Mark Lane Hamlin <mhamlin0621@proton.me>
2026-03-14 11:28:42 -0500

    5: Creating the grass background

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill("purple")
+        screen.fill((0,135,62))
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
