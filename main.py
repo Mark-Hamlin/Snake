9c89cfa1172ebbba81479c3ea64cf16fa7c65db3
Mark-Hamlin <mhamlin0621@proton.me>
2026-03-15 16:33:38 -0500

    7:Getting the snake to move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGTH
+from snake import *
 def main():
     print("Hello to my first solo project")
     print("I am making the game snake")
@@ -8,11 +9,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGTH))
     clock = pygame.time.Clock()
     running = True
-    while running:
+    x_value = SCREEN_WIDTH//2
+    y_value = SCREEN_HEIGTH//2
+    while running: 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            if event.type == pygame.QUIT: running = False
+
+        snake = Snake(x_value,y_value,50,50,"red") 
         screen.fill((0,135,62))
+        snake.draw(screen)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            y_value -= 5
+        if keys[pygame.K_s]:
+            y_value += 5
+        if keys[pygame.K_a]:
+            x_value -= 5
+        if keys[pygame.K_d]:
+            x_value += 5
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
--- snake.py
+++ snake.py
@@ -1,7 +1,12 @@
 import pygame
 
 class Snake:
-    def __init__(self,width,height,color):
+    def __init__(self,x,y,width,height,color):
         self.width = width
         self.height = height
         self.color = color
+        self.x = x
+        self.y = y
+    def draw(self,screen):
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.height,self.width))
+        
